fda84040bc0fc4209747251b1d7665c73263274d
Ethan Carvalho <524ecarvalho@frhsd.com>
2024-01-26 13:18:25 -0500

    added all the schedules

--- script.js
+++ script.js
@@ -3,8 +3,22 @@ $(document).ready( function () {
     // make a GET request to the JSON API using JQuery method
     $.getJSON("schedule.json", function (data) {
         //global variable to store data
-        let scheduleData = data.schedule;
+        let myScheduleData = data.mySchedule;
+         let adilScheduleData = data.adilSchedule;
+          let samScheduleData = data.samSchedule;
     populateTable(scheduleData);
+
+//event listen for changes to the dropdown menu
+$('#day-selector').on("change", function () {
+    //Get selected day
+    let selectedDay = $(this).val();
+
+    //filter schedule data based on the selected day
+    let filteredData = scheduleData.filter( function (schedule){
+        return selectedDay === 'all' || schedule.days.includes(selectedDay);
+    })
+})
+
     }) ;
 
     function populateTable(scheduleData){
